0e2272d5924a40b3fbb2665887076f6725d7d23a
geumpil <86139036+geumpil@users.noreply.github.com>
2023-08-03 12:54:25 +0900

    230803_class

---
+++ welcomeF&D/js/bannerAnimation.js
@@ -0,0 +1,8 @@
+const animationBar = document.querySelector('.bar-anuimation');
+const circlePlus = document.querySelector('.plus');
+const animationText = document.querySelector('.text-animation');
+const contentText = ['Finance', 'Service', 'Data', 'Tech', 'Experience'];
+
+function playBar() {
+    animationBar.style.width = '100px';
+}
