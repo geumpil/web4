990ba0a4f61693354a57b13b7dd3f37111b58d31
geumpil <86139036+geumpil@users.noreply.github.com>
2023-11-02 14:02:04 +0900

    231102_webproject

--- project(Bang&Olufsen)/js/scroll_act.js
+++ project(Bang&Olufsen)/js/scroll_act.js
@@ -1,15 +1,18 @@
 /* scroll bg-color change*/
 const bgWhiteLocation = document.querySelector('.durability'); 
-const bgBlackLocation = document.querySelector('.home-textarea');
+const bgBlackLocation = document.querySelector('.view-area');
+const textColor = document.querySelector('.home-textarea')
 
 window.addEventListener('scroll', () => {
     console.log(bgBlackLocation.getBoundingClientRect());
-    if(bgWhiteLocation.getBoundingClientRect().top < 0 && bgBlackLocation.getBoundingClientRect().bottom < 0) {
-        // document.body.style.backgroundColor = 'white';
-        document.body.style.transition = '1s';
+    if(bgWhiteLocation.getBoundingClientRect().top < 0 && bgBlackLocation.getBoundingClientRect().top > 0) {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.transition = '0.4s';
+        textColor.style.color = 'black';
     } 
     else{
         document.body.style.backgroundColor = 'black';
+        textColor.style.color = 'white';
     }
 }) 
 
@@ -28,3 +31,16 @@ const io = new IntersectionObserver((e, observer) => {
 })
 
 document.querySelectorAll('.scroll-act').forEach((act) => io.observe(act));
+
+
+
+const foldContainer = document.querySelector('.fold-container');
+
+window.addEventListener('scroll',()=>{
+    if(foldContainer.getBoundingClientRect().top < 0 + window.innerHeight) {
+        foldContainer.classList.add('unfolded')
+    }else {
+        
+        foldContainer.classList.remove('unfolded')
+    }
+})
