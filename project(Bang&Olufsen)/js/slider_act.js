f722eec3e3223b8cd7086feb7158cc55d4f85787
geumpil <86139036+geumpil@users.noreply.github.com>
2023-11-10 14:59:40 +0900

    231110_webproject

--- project(Bang&Olufsen)/js/slider_act.js
+++ project(Bang&Olufsen)/js/slider_act.js
@@ -4,12 +4,16 @@ const slideImgList = document.querySelectorAll('.slide-image-view');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 const realSlideCount = slideImgList.length;
+const slideArea = document.querySelector('.slider-visual-area')
 const viewCount = 3;
 
 let slideSize = '1000px';
 let gapSize = 200;
 
 let index = 5;
+
+
+let loopInterval;
 
 for(let i = 0 ; i < realSlideCount ; i ++) {
 
@@ -37,6 +41,8 @@ apply(false);
 function apply(animation){
     slideImg.style.transform=  `translateX(calc((-${slideSize} - ${gapSize}px) * ${index}))`
     buttonUpdate();
+
+    resetInterval();
 
     if(animation){
         slideImg.style.transition = `1s`
@@ -118,11 +124,32 @@ function makeClone() {
     // 복사한것 붙여넣기(뒤)
 }
 
-// prev, next button
+// prev, next button and autoSlide
 
     let moveAble = true;
 
-    prevBtn.addEventListener('click', ()=> {
+    prevBtn.addEventListener('click', prev)
+    nextBtn.addEventListener('click',next)
+
+    function resetInterval(){
+        clearInterval(loopInterval);
+
+        loopInterval = setInterval(() => {
+            next();
+        }, 4000);
+    }
+
+    // slideArea.addEventListener('mouseover', () => {
+    //     clearInterval(loopInterval);
+    // });
+
+    // slideArea.addEventListener('mouseout', () => {
+    //     loopInterval = setInterval(() => {
+    //         next();
+    //     }, 4000);
+    // });
+
+    function prev() {
         if(moveAble){
             moveAble=false;
             index--;
@@ -135,24 +162,67 @@ function makeClone() {
                 apply(false);
             }
             moveAble=true;
-        }, 1000);
-    }
-    })
-
-    nextBtn.addEventListener('click',()=>{
-       if(moveAble){
-        moveAble = false; 
-        // console.log(realSlideCount.length)
-        index++;
-        apply(true)
-        setTimeout(() => {
-
-            if(index>realSlideCount-1+viewCount){
-                index=viewCount;
-                apply(false);
-            }
-            moveAble=true;
         }, 1000);}
+    }
+
+    function next() {
+        if(moveAble){
+            moveAble = false; 
+            // console.log(realSlideCount.length)
+            index++;
+            apply(true)
+            setTimeout(() => {
+    
+                if(index>realSlideCount-1+viewCount){
+                    index=viewCount;
+                    apply(false);
+                }
+                moveAble=true;
+            }, 1000);}
+    }
+
+    //스와이프 시작지점 끝지점 초기화
+
+    let startPoint = 0;
+    let endPoint = 0;
+
+    let isClicked = false
+
+    slideArea.addEventListener('mousedown', (e) => {
+        if(moveAble){
+            e.preventDefault();
+            startPoint = e.pageX;
+            console.log(startPoint);
+            isClicked = true;
+        }
+
+    });
+
+    slideArea.addEventListener('mousemove', (e) =>{
+        if(isClicked) {
+            console.log('드래그중')
+            const 이동거리 = e.pageX - startPoint;
+            slideImg.style.transition = `0s`
+            slideImg.style.transform=  `translateX(calc((-${slideSize} - ${gapSize}px) * ${index} + ${이동거리}px))`
+        }
     })
 
+    slideArea.addEventListener('mouseup', (e) => {
+        isClicked = false
+        endPoint = e.pageX;
+        console.log(endPoint);
 
+        // start 100,
+        // end 200일때
+        // x방향으로 + 100만큼 이동한것.
+        // 이동거리 = end - start > 100 일떄 prev
+        // 이동거리 = end - start < -100 일떄 next
+        //
+        if(endPoint - startPoint > 150) {
+            prev();
+        } else if (endPoint - startPoint < -150) {
+            next();
+        }else {
+            apply(true);
+        }
+    });
